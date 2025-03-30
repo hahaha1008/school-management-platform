724ad34ab36092633e577e9f68cb36ff1a2314b5
hahaha1008 <weiaisuli1314@163.com>
2025-03-30 17:17:46 -0400

    Add Render deployment configuration

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
-# exit on error
 set -o errexit
 
+# Force platform to ensure compatibility
+bundle config set --local force_ruby_platform true
+bundle config set --local without development test
 bundle install
+
+# Asset compilation
 bundle exec rake assets:precompile
 bundle exec rake assets:clean
+
+# Database setup
 bundle exec rake db:migrate
+bundle exec rake db:seed
