7eb351fadfb9bc23f4c8353a23617a606584769e
shis20 <shis20@mcmaster.ca>
2025-04-11 12:49:43 -0400

    Disable Active Storage background jobs

--- config/initializers/active_storage.rb
+++ config/initializers/active_storage.rb
@@ -1,3 +1,6 @@
-# config/initializers/active_storage.rb
+# Disable background analysis for Active Storage
 Rails.application.config.active_storage.queues.analysis = nil
 Rails.application.config.active_storage.queues.purge = nil
+
+# Make variant processor synchronous (for image resizing without background jobs)
+Rails.application.config.active_storage.variant_processor = :mini_magick
